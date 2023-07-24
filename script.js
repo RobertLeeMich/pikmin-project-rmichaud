f0ac7446bf6941bbbec039062ea28b8d35285f91
Robert Michaud <guildwarsguy123@yahoo.com>
2023-07-24 19:30:00 -0400

    Made a grid layout that I am happy with. Grid is super easy to use using grid-template-areas

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //Beginning of the Pikmin-Project
+const body = document.querySelector("body")
+
+body.style.backgroundImage = "url(image/thefinaltrialmap.png)"
+body.style.backgroundSize = "100vw"
+body.style.backgroundRepeat = "no-repeat"
+body.style.backgroundColor = "black"
+
+
+
+
+
 
