ab3f0cd57d17039d0019621a0438e7a126505469
Robert Michaud <guildwarsguy123@yahoo.com>
2023-07-24 21:22:01 -0400

    last working version of css grid

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-//Beginning of the Pikmin-Project
 const body = document.querySelector("body")
-
+//Beginning of the Pikmin-Project
+//BACKGROUND STYLING
 body.style.backgroundImage = "url(image/thefinaltrialmap.png)"
 body.style.backgroundSize = "100vw"
 body.style.backgroundRepeat = "no-repeat"
 body.style.backgroundColor = "black"
+
+//SELECTORS
+const fireDiv = document.getElementsByClassName("i1")
+const waterDiv = document.getElementsByClassName("i2")
+const olimarsShip = document.getElementsByClassName("i3")
+const electricDiv = document.getElementsByClassName("i4")
+const div4 = document.getElementsByClassName("i5")
+const div6 = document.getElementsByClassName("i6")
+const div7 = document.getElementsByClassName("i7")
+const div8 = document.getElementsByClassName("i8")
+const pikminNumbers = document.getElementsByClassName("i9")
+
+//SHIP MANIPULATION
+const damagedShip = document.getElementById("damaged-ship")
+const repairedShip = document.getElementById("repaired-ship")
+repairedShip.style.display = "none" //Flip this later when ship is repaired
+
+//INFORMATION MODAL ABOUT OLIMAR'S SHIP
 
 
 
