bad06f1a964f9bddc894a60ed84e3d1086c58701
Robert Michaud <guildwarsguy123@yahoo.com>
2023-07-27 14:35:22 -0400

    got the overlay working to move to the next day, and remove it on next day, as well as saving the day value across rounds

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ let itemSelect = document.querySelector(".i1")
 const olimarShip = document.querySelector(".i2")
 const resetButton = document.querySelector(".i3")
 const olimarBox = document.querySelector(".i4")
-const pikminTotal = document.getElementById("pikmin-total")
+// const pikminTotalBox = document.getElementById("pikmin-total")
 const nextDayOverlay = document.getElementById("next-day-overlay")
 const nextDayButton = document.getElementById("next-day-button")
+const dayTimer = document.getElementById("day-timer")
 // let contentDiv = document.querySelector(".i5")
 
 // //SHOWING DIV IN MIDDLE OF SCREEN FOR INFORMATION ABOUT ITEM CLICKED (NULLIFIED FOR NOW)
@@ -25,21 +26,57 @@ const nextDayButton = document.getElementById("next-day-button")
 // }
 
 //DISABLE ELEMENTS DIV/NEXT DAY BUTTON
-function nextDayOverlayShowHide() {
+function nextDayOverlayShow() {
     if (nextDayOverlay.style.display = "none"){
         nextDayOverlay.style.display = "block"
     }
 }
 
+function nextDayOverlayHide(){
+    if (nextDayOverlay.style.display = "block"){
+        nextDayOverlay.style.display = "none"
+    }
+}
+
+//SEE IF USER CLICKED TWICE ON THE DIV
+let clickCounter = 0
+itemSelect.addEventListener("click", (e) => {
+    clickCounter++
+    if (clickCounter === 2){
+        nextDayOverlayShow()
+        clickCounter = 0
+    }
+})
+
 //ROUND SETUP
 let dayCounter = 0
 let shipPartCounter = 0
+let monsterCounter = 0
+dayTimer.innerHTML = `<p>You have taken ${dayCounter}/6 days!</p>`
+dayTimer.class = "save-timer"
 
 nextDayButton.addEventListener("click", (e) => {
-    daycounter++
+    dayCounter++
+    updateDayTimer()
+    nextDayOverlayHide()
+    
+    
 })
 
+//divrefresh/overlayremoval code
 
+
+function updateDayTimer() {
+    if (dayCounter === 6 && shipPartCounter === 6){
+        dayTimer.innerHTML = `<p> Click Olimar's ship to leave the planet! </p>`;
+    } else {
+        dayTimer.innerHTML = `<p>You have taken ${dayCounter}/6 days!</p>`;
+    }
+}
+
+function saveDayTimer() {
+    localStorage.setItem("dayCounter", dayTimer)
+}
 
 // PIKMIN COUNTER/PIKMIN MANIPULATION
 const pikmin =  {
@@ -61,55 +98,79 @@ resetButton.addEventListener("click", (e) => {
 
 //PLAYER SELECTS ITEM, ITEM GETS ADDED TO ARRAY, IF PLAYER HAS 3 MONSTERS THEY LOSE, IF PLAYER HAS 3 SHIP PARTS, THEY WIN
 const imageArr = [
-    "image/armored-cannon-beetle.png", "image/emperor-bulblax.png", "image/small-bulborb.png", "image/yellow-wollywog.png", "image/shipparts/secret-safe.png", "image/shipparts/chronos-reactor.png", "image/shipparts/ionium-jet.png", "image/shipparts/main-engine.png", "image/shipparts/nova-blaster.png", "image/shipparts/radiation-canopy.png"
+    "image/armored-cannon-beetle.png", "image/emperor-bulblax.png", "image/red-1-pellet.png", "image/red-5-pellet.png", "image/small-bulborb.png", "image/yellow-wollywog.png", "image/shipparts/secret-safe.png", "image/shipparts/chronos-reactor.png", "image/shipparts/ionium-jet.png", "image/shipparts/main-engine.png", "image/shipparts/nova-blaster.png", "image/shipparts/radiation-canopy.png"
 ]
 
 //PICTURE REQUIREMENT HANDLING CONDITIONS
-const imageRequirements = {
+let imageRequirements = {
     "image/armored-cannon-beetle.png": {
-        isMonster: true, //DEFINE MONSTER BY THIS PROPERTY
-        shipPart: false 
+        isMonster: true,
+        shipPart: false,
+        addPikmin: false
 
     },
     "image/emperor-bulblax.png": {
-        isMonster: true, //DEFINE MONSTER BY THIS PROPERTY
-        shipPart: false 
+        isMonster: true, 
+        shipPart: false,
+        addPikmin: false 
 
     },
     "image/small-bulborb.png": {
-        isMonster: true, //DEFINE MONSTER BY THIS PROPERTY
-        shipPart: false 
+        isMonster: true, 
+        shipPart: false,
+        addPikmin: false 
 
     },
     "image/yellow-wollywog.png": {
-        isMonster: true, //DEFINE MONSTER BY THIS PROPERTY
-        shipPart: false 
+        isMonster: true,
+        shipPart: false,
+        addPikmin: false 
 
     },
     "image/shipparts/secret-safe.png": {
         isMonster: false,
-        shipPart: true //DEFINE SHIP PART BY THIS PROPERTY
+        isShipPart: true,
+        addPikmin: false
     }, 
     "image/shipparts/chronos-reactor.png":{
         isMonster: false,
-        shipPart: true
+        isShipPart: true,
+        addPikmin: false
     },
     "image/shipparts/ionium-jet.png":{
         isMonster: false,
-        shipPart: true
+        isShipPart: true,
+        addPikmin: false
     },
     "image/shipparts/main-engine.png":{
         isMonster: false,
-        shipPart: true
+        isShipPart: true,
+        addPikmin: false
     },
     "image/shipparts/nova-blaster.png":{
         isMonster: false,
-        shipPart: true
+        isShipPart: true,
+        addPikmin: false
     },
     "image/shipparts/radiation-canopy.png":{
         isMonster: false,
-        shipPart: true
-    }
+        isShipPart: true,
+        addPikmin: false
+    },
+    "image/red-5-pellet.png":{
+        isMonster: false,
+        shipPart: false,
+        addPikmin: function () {
+            return pikmin.numberOf += 5
+        }
+    },
+    "image/red-1-pellet.png":{
+        isMonster: false,
+        shipPart: false,
+        addPikmin: function () {
+            return pikmin.numberOf += 1
+        }
+    },
 }
 //ADDS IMAGE TO DIV FOR USER TO CLICK
 function imageIteration(){
@@ -126,14 +187,6 @@ function imageIteration(){
 }
 imageIteration()
 
-//SEE IF USER CLICKED TWICE ON THE DIV
-let clickCounter = 0
-itemSelect.addEventListener("click", (e) => {
-    clickCounter++
-    if (clickCounter === 2){
-        nextDayOverlayShowHide()
-    }
-})
 
 
 //RANDOMIZED IMAGES IN THE DIV
@@ -146,7 +199,25 @@ randomizedImage.addEventListener("click", (e) => {
     let imageDetails = imageRequirements[imageKey]
     console.log(e) //div-image
     userWinOrLoseArr.push(imageDetails)
-    console.log(userWinOrLoseArr) //has the objects in it
+    console.log(userWinOrLoseArr) //HAS THE OBJECTS IN IT
+
+    //CHECK IMAGE, INCREMENT COUNTERS ON CLICK
+    let isMonster = imageDetails.isMonster
+    let isShipPart = imageDetails.isShipPart
+    let addPikmin = imageDetails.addPikmin
+    if (isMonster === true){
+        monsterCounter++
+        console.log(monsterCounter) //INCREMENTS MONSTERCOUNTER
+    } else if (isShipPart === true){
+        shipPartCounter++
+        console.log(shipPartCounter)
+    } else if (addPikmin){
+        addPikmin()
+        pikmin.render()
+        console.log(pikmin.numberOf) //INCREMENTS PIKMIN TOTAL ON SCREEN
+    }
 })
+
+
 
 
